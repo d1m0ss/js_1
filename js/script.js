f51124e2f1fc0c503e588be671174df57a5ccd90
Dim0ss <dima.volkov1998@gmail.com>
2020-02-29 07:48:47 +0300

    first_js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var butonCenter = document.getElementById("buton__center");
+var bgrAll = document.getElementById("bgrund");
+
+function styleFlip(){
+    if(butonCenter.style.backgroundColor == "saddlebrown"){
+        bgrAll.style.backgroundColor = "purple";
+    }
+}
